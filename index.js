51177edd107439e50c7f5ea615b5604dd3187aad
shohel-ranaC <akashshohel422@gmail.com>
2023-06-07 12:09:24 -0700

    yoga zone server site basic setup is done

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
