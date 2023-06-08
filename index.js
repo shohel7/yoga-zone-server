9ae63536110a77c7c355f6e2954e089c011d4ac3
shohel-ranaC <akashshohel422@gmail.com>
2023-06-07 18:28:47 -0700

    added popular class api is completed

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clv72st.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const popularClassCollection = client
+      .db("yogaDb")
+      .collection("popularClasses");
+    const popularInstructorCollection = client
+      .db("yogaDb")
+      .collection("popularInstructors");
+
+    app.get("/popularClasses", async (req, res) => {
+      const result = await popularClassCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Yoga Zone is running");
+});
+
+app.listen(port, () => {
+  console.log(`Yoga Zone is running at port ${port}`);
+});
